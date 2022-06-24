6988d0f8af9d04d3ee54d046169b9c2f60194ef1
Сергей Брайчук <sergejbrajcuk@MacBook-Pro-Sergej.local>
2022-06-24 15:54:00 +0300

    add form

--- src/App.vue
+++ src/App.vue
@@ -4,20 +4,27 @@
       <h1 class="header__title"> {{ title }} </h1>
       <div class="header__selected-wrapper">
         <select class='header__selected-options' v-model="selected">
-          <option disabled value="default">По умолчанию</option>
+          <option disabled value="">По умолчанию</option>
           <option class="header__selected-option" v-for="item in options" :key="item.value" :value="item.value"> {{ item.select }} </option>
         </select>
       </div>
     </div>
+    <main class="main">
+      <aside class="main__form-wrapper">
+        <form-vue :inputValue="inputValue"/>
+      </aside>
+      <aside class="main__content-wrapper"></aside>
+    </main>
   </header>
 </template>
 
 <script>
+import FormVue from './components/formVue.vue'
 
 export default {
   name: 'App',
   components: {
-    
+    FormVue
   },
   data() {
     return {
@@ -29,20 +36,45 @@ export default {
         { select: 'По цене min', value: 'min' },
         { select: 'По наименованию', value: 'name' },
       ],
+      inputValue: [
+        { type: 'text', placeholder: 'Введите наименование товара', title: 'Наименование товара'},
+        { type: 'text', placeholder: 'Введите описание товара', title: 'Описание товара'},
+        { type: 'email', placeholder: 'Введите ссылку', title: 'Ссылка на изображение товара'},
+        { type: 'number', placeholder: 'Введите цену', title: 'Цена товара'},
+      ]
     }
   }
 }
 </script>
 
 <style lang="scss">
+  @import url('https://fonts.googleapis.com/css2?family=Inter:wght@600&family=Source+Sans+Pro:wght@400;600&display=swap');
   *, *::after, *::before {
     box-sizing: border-box;
     margin: 0;
     padding: 0;
+    font-family: 'Source Sans Pro', sans-serif;
+    color: #3F3F3F;
+    font-weight: 400;
+  }
+  body {
+    max-width: 1440px;
+    margin: 0 auto;
+  }
+  button {
+    font-family: 'Inter', sans-serif;
+    cursor: pointer;
+  }
+  input {
+    outline: none;
+    border: none;
   }
   .header {
     &__inner {
-
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      padding: 32px 32px 16px 32px;
     }
     &__title {
 
@@ -56,6 +88,12 @@ export default {
       box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
       border-radius: 4px;
       padding: 10px 0px 10px 16px;
+      cursor: pointer;
     }
+  }
+  .main {
+    padding: 0 32px;
+    display: flex;
+    position: relative;
   }
 </style>
---
+++ src/components/formVue.vue
@@ -0,0 +1,139 @@
+<template>
+  <form action="" class="form" @submit.prevent="onSubmit">
+    <div class="form__input-wrapper" v-for="(item, id ) in inputValue" :key="id">
+      <div class="form__input-title-wrapper">
+        <p class="form__input-title">
+        {{ item.title }}
+        </p>
+        <div class="form__input-title-dot"></div>
+      </div>
+      <input class="form__input" :type="item.type" :placeholder="item.placeholder" :id="id"/>
+      <textarea class="form__textarea" name="" id="" :placeholder="item.title"></textarea>
+      <label class="form__label" :for="id">Поле является обязательным</label>
+    </div>
+    <button class="form__btn">Добавить товар</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'FormVue',
+  props: {
+    inputValue: {
+      type: Array,
+      default: []
+    }
+  },
+  methods: {
+    onSubmit() {
+      console.log('great!!!')
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+  .form {
+    padding: 24px;
+    background: #FFFEFB;
+    box-shadow: 0px 20px 30px rgba(0, 0, 0, 0.04), 0px 6px 10px rgba(0, 0, 0, 0.02);
+    border-radius: 4px;
+    position: fixed;
+    top: 83px;
+    &__input-wrapper {
+    display: flex;
+    flex-direction: column;
+    }
+    &__input-wrapper > textarea {
+      display: none;
+    }
+    &__input-wrapper:nth-child(2) > &__input-title-wrapper > &__input-title-dot {
+      display: none;
+    }
+    &__input-wrapper:nth-child(2) > input {
+      height: 108px;
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+    }
+    &__input-wrapper:nth-child(2) > input::placeholder {
+      color: #FF8484;
+      display: flex;
+      flex-direction: column;
+    }
+    &__input-wrapper:nth-child(2) > input {
+      display: none;
+    }
+    &__input-wrapper:nth-child(2) > textarea {
+      display: flex;
+      height: 108px;
+      padding: 10px 16px;
+      background: #FFFEFB;
+      box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
+      border-radius: 4px;
+      border: none;
+      resize: none;
+      outline: none;
+      margin-bottom: 16px;
+    }
+    &__input-wrapper:nth-child(2) > textarea::placeholder {
+      font-weight: 400;
+      font-size: 12px;
+      line-height: 15px;
+      color: #B4B4B4;
+    }
+    &__input-wrapper:nth-child(2) > label {
+      display: none;
+    }
+    &__input-title-wrapper {
+      display: flex;
+    }
+    &__input-title {
+      font-weight: 400;
+      font-size: 10px;
+      line-height: 13px;
+      letter-spacing: -0.02em;
+      color: #49485E;
+      margin-bottom: 4px;
+    }
+    &__input-title-dot {
+      height: 4px;
+      width: 4px;
+      background: #FF8484;
+      border-radius: 2px;
+    }
+    &__input {
+      padding: 10px 16px 11px 16px;
+      background: #FFFEFB;
+      box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
+      border-radius: 4px;
+      line-height: 15px;
+    }
+    
+    &__input::placeholder {
+      font-size: 12px;
+      color: #B4B4B4;
+    }
+    &__label {
+      font-size: 8px;
+      line-height: 10px;
+      letter-spacing: -0.02em;
+      color: #FF8484;
+      padding: 4px 0 2px 0;
+    }
+    &__btn {
+      background: #EEEEEE;
+      border-radius: 10px;
+      width: 100%;
+      margin-top: 8px;
+      padding: 10px 96px;
+      border: none;
+      color: #B4B4B4;
+      font-weight: 600;
+      font-size: 12px;
+      line-height: 15px;
+      letter-spacing: -0.02em;
+    }
+  }
+  
+</style>
